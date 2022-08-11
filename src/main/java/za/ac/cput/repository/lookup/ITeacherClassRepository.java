ef8a7f6437271d1d809bd7bfb58f6faebe478dc1
miketyo <mikesomelezotyolani41@gmail.com>
2022-08-11 21:38:19 +0200

    Added Repository for entities.

---
+++ src/main/java/za/ac/cput/repository/lookup/ITeacherClassRepository.java
@@ -0,0 +1,14 @@
+package za.ac.cput.repository.lookup;
+/* Author : Mike Somelezo Tyolani
+ *  Student Number: 220187568
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.lookup.TeacherClass;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ITeacherClassRepository extends JpaRepository<TeacherClass, String> {
+    Optional<TeacherClass> findTeacherClassByRoomId(String roomId);
+}
---
+++ src/main/java/za/ac/cput/repository/user/IIncidentsRepository.java
@@ -0,0 +1,14 @@
+package za.ac.cput.repository.user;
+/* Author : Mike Somelezo Tyolani
+ *  Student Number: 220187568
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.user.Incidents;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IIncidentsRepository extends JpaRepository<Incidents, String> {
+    Optional<Incidents> findByIncidentId(String incidentId);
+}
